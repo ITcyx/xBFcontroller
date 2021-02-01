a17365aa0b2da1880fbbd71ea5d342b32449a8cc
Andy Cai <1781953879@qq.com>
2021-02-01 14:26:02 +0800

    modified bfreader

--- code/bfcontroller.h
+++ code/bfcontroller.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 #ifndef XBFCONTROLLER_H
 #define XBFCONTROLLER_H
 #include "code.h"
@@ -7,6 +7,7 @@
 
 namespace x
 {
+	// bfreader 读取二进制文件
 	class bfreader
 	{
 	private:
@@ -24,7 +25,7 @@ namespace x
 		bool set_file_name(std::string file_name, int cluster = 4096);  // 设置将要读取的文件名，若文件不存在或无法读取，返回false
 		bool set_cluster(int cluster = 4096);  // 设置簇的大小，不会改变当前的buffer池的内容与大小，将在下一次读取中采用新的簇的值。若输入的值小于等于零，将不会更新簇的大小且返回false
 		std::string read(int length=1);  // 从当前读取的位置开始，读取指定长度的内容。若指定长度超过文件剩余大小，将只返回文件剩余大小的内容；若指定的文件因某些原因无法读取，将返回空字符串
-		int size(int unit = code::B);  // unit ，单位，默认值为Byte。本函数为获取当前文件的大小
+		int get_size(int unit = code::B);  // unit ，单位，默认值为Byte。本函数为获取当前文件的大小
 		int cluster_value();  // 返回cluster的值
 		bool clean();  //
 		bool close();
