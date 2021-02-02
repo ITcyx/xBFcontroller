d517089d3b79225e12e629cca0305631535847fe
Andy Cai <1781953879@qq.com>
2021-02-02 08:46:17 +0800

    modified bfreader

--- code/bfcontroller.cpp
+++ code/bfcontroller.cpp
@@ -7,10 +7,13 @@ x::bfreader::bfreader(int cluster)
 		cluster = 4096;
 	c = cluster;
 	cn = -1;
+	s = -1;
 	buffer = new unsigned char[c];
 	bi = -1;
 	bj = -1;
 	status = code::INIT;
+	p = -1;
+	s = -1;
 }
 
 x::bfreader::bfreader(std::string file_name, int cluster)
@@ -27,68 +30,29 @@ x::bfreader::bfreader(std::string file_name, int cluster)
 	{
 		fn = "";
 		status = code::INIT;
+		p = -1;
+		s = -1;
 	}
 	else
 	{
 		fn = file_name;
 		status = code::NORMAL;
+		bf.seekg(0, std::ios::end);
+		s = bf.tellg();
+		bf.seekg(0, std::ios::beg);
+		p = 0;
 	}
 }
 
 x::bfreader::~bfreader()
 {
+	bf.close();
+	bf.clear();
 	delete[]buffer;
 }
 
-
 bool x::bfreader::set_filename(std::string file_name, int cluster)
 {
-	if (fn == "")
-	{
-		bf.open(file_name);
-		if (!bf.good())
-		{
-			bi = -1;
-			bj = -1;
-			cn = -1;
-			if (cluster <= 0)
-				cluster = 4096;
-			if (c != cluster)
-			{
-				c = cluster;
-				delete[]buffer;
-				buffer = new unsigned char[c];
-			}
-			status = code::INIT;
-			return false;
-		}
-	}
-	else
-	{
-		bf.close();
-		fn = "";
-		bf.open(file_name);
-		if (!bf.good())
-		{
-			bi = -1;
-			bj = -1;
-			cn = -1;
-			if (cluster <= 0)
-				cluster = 4096;
-			if (c != cluster)
-			{
-				c = cluster;
-				delete[]buffer;
-				buffer = new unsigned char[c];
-			}
-			status = code::INIT;
-			return false;
-		}
-	}
-	fn = file_name;
-	bi = -1;
-	bj = -1;
-	cn = -1;
 	if (cluster <= 0)
 		cluster = 4096;
 	if (c != cluster)
@@ -97,6 +61,32 @@ bool x::bfreader::set_filename(std::string file_name, int cluster)
 		delete[]buffer;
 		buffer = new unsigned char[c];
 	}
+	cn = -1;
+	if (fn == "")
+		bf.open(file_name, std::ios::binary);
+	else
+	{
+		bf.close();
+		bf.clear();
+		fn = "";
+		bf.open(file_name, std::ios::binary);
+	}
+	if (!bf.good())
+	{
+		bi = -1;
+		bj = -1;
+		s = -1;
+		p = -1;
+		status = code::INIT;
+		return false;
+	}
+	fn = file_name;
+	bi = -1;
+	bj = -1;
+	bf.seekg(0, std::ios::end);
+	s = bf.tellg();
+	bf.seekg(0, std::ios::beg);
+	p = 0;
 	status = code::NORMAL;
 	return true;
 }
@@ -105,6 +95,69 @@ bool x::bfreader::set_cluster(int cluster)
 {
 	if (cluster <= 0)
 		return false;
-	cn = cluster;
+	if (cluster == c)
+		return true;
+	if (fn == "")
+	{
+		delete[]buffer;
+		buffer = new unsigned char[cluster];
+		c = cluster;
+		return true;
+	}
+	c = cn;
 	return true;
+}
+
+
+bool x::bfreader::set_position(long long position)
+{
+	if (fn == "")
+		return false;
+	if (position > s || position < 0)
+		return false;
+	if (bi < 0)
+	{
+		p = position;
+		bf.seekg(p, std::ios::beg);
+		return true;
+	}
+	bi = -1;
+	bj = -1;
+	p = position;
+	bf.seekg(p, std::ios::beg);
+	return true;
+}
+
+long long x::bfreader::get_size(int unit)
+{
+	if (s < 0)
+		return s;
+	if (unit == code::B)
+		return s;
+	if (unit >= code::KIB&&unit <= code::PIB)
+		return (s >> (10 * (code::KIB - 1023)));
+	if (unit >= code::KB&&unit <= code::PB)
+	{
+		long long t = 1000;
+		for (int i = 0; i < (code::KB - 1000); ++i)
+			t *= 1000;
+		return s / t;
+	}
+	return -1;
+}
+
+int x::bfreader::get_cluster()
+{
+	return c;
+}
+
+long long x::bfreader::get_position()
+{
+	return p;
+}
+
+x::barray x::bfreader::read(long long length, long long positon)
+{
+	if(status==code::INIT)
+		return barray();
 }
--- code/bfcontroller.h
+++ code/bfcontroller.h
@@ -15,8 +15,8 @@ namespace x
 		std::ifstream bf;
 		std::string fn;
 		unsigned char *buffer;
-		int c, status, bi, bj;
-		long long p, cn;
+		int c, status, bi, bj, cn;
+		long long p, s;
 	public:
 		// 构造函数、析构函数
 		bfreader(int cluster = 4096);  // cluster ，簇，默认值为4096KiB，ifstream一次读取的大小为一个簇的大小
@@ -27,7 +27,7 @@ namespace x
 		bool set_filename(std::string file_name, int cluster = 4096);  // 设置将要读取的文件名，若文件不存在或无法读取，返回false
 		bool set_cluster(int cluster = 4096);  // 设置簇的大小，不会改变当前的buffer池的内容与大小，将在下一次读取中采用新的簇的值。若输入的值小于等于零，将不会更新簇的大小且返回false
 		bool set_position(long long position);  // position 设置读取位置，当超过文件大小或为负数或者其他无法操作文件等情况，返回false
-		long long get_size(int unit = code::B);  // unit ，单位，默认值为Byte。本函数为获取当前文件的大小
+		long long get_size(int unit = code::B);  // unit ，单位，默认值为Byte。本函数为获取当前文件的大小。如果为负数，即未打开文件
 		int get_cluster();  // 返回cluster的值
 		long long get_position();  // 返回当前读到到的字符相对文件头的距离（0即正好读取到文件头，-1即代表没加载文件)
 		barray read(long long length = 1, long long position = -1);  // length ，从当前读取的位置开始，读取指定长度的内容。若指定长度超过文件剩余大小，将只返回文件剩余大小的内容；若指定的文件因某些原因无法读取，将返回空字符串。position ，设置读取位置，如果为负数或超过当前文件大小，将从当前位置读起。默认值-1
